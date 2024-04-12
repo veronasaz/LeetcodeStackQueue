835354534002b36655f5411da7bcefbd80e4bac0
veronasaz <147612156+veronasaz@users.noreply.github.com>
2024-04-12 12:50:40 +0300

    Implement Queue using Stacks

---
+++ Implement Queue using Stacks/solution.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            return None
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        if self.top is None:
+            return None
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def push(self, x: int) -> None:
+        self.stack1.push(x)
+
+    def pop(self) -> int:
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if self.stack2.is_empty():
+            while not self.stack1.is_empty():
+                self.stack2.push(self.stack1.pop())
+        return self.stack2.peek()
+
+    def empty(self) -> bool:
+        return self.stack1.is_empty() and self.stack2.is_empty()
+
+# Your MyQueue object will be instantiated and called as such:
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+param_2 = obj.pop()
+obj.push(3)
+param_3 = obj.peek()
+param_4 = obj.empty()
